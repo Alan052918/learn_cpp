4ae3d8b2900daea52137fb8fa6b63ace07b063f7
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2023-01-08 21:33:51 -0500

    Add more sequence containers demo

--- containers/sequence_demo.cc
+++ containers/sequence_demo.cc
@@ -9,8 +9,56 @@
 // #include "templates/common/print_containers.h"
 #include "meta/common/print_containers.h"
 
-int main(int argc, char const *argv[]) {
+void VectorDemo() {
+  std::cout << std::endl << " --- VectorDemo --- " << std::endl;
+
   std::vector<std::string> payroll{"John", "Jane", "Jack", "Jill"};
-  // templates::common::Print(payroll);
   meta::common::Print("payroll", payroll);
+
+  std::cout << std::endl << " --- VectorDemo exit scope --- " << std::endl;
+}
+
+void DequeDemo() {
+  std::cout << std::endl << " --- DequeDemo --- " << std::endl;
+
+  std::deque<std::string> names{"Alice", "Bob", "Charlie", "Dave"};
+  meta::common::Print("names", names);
+
+  std::cout << std::endl << " --- DequeDemo exit scope --- " << std::endl;
+}
+
+void ListDemo() {
+  std::cout << std::endl << " --- ListDemo --- " << std::endl;
+
+  std::list<std::string> names{"Alice", "Bob", "Charlie", "Dave"};
+  meta::common::Print("names", names);
+
+  names.push_back("Eve");
+  meta::common::Print("names", names);
+
+  // transfer element i from c to *this before p
+  names.splice(names.end(), names, names.begin());
+  meta::common::Print("names", names);
+
+  // transfer elements in range [f, l) from c to *this before p
+  names.splice(names.end(), names, names.begin(), std::next(names.begin(), 2));
+  meta::common::Print("names", names);
+
+  std::cout << std::endl << " --- ListDemo exit scope --- " << std::endl;
+}
+
+void ForwardListDemo() {
+  std::cout << std::endl << " --- ForwardListDemo --- " << std::endl;
+
+  std::forward_list<std::string> names{"Alice", "Bob", "Charlie", "Dave"};
+  meta::common::Print("names", names);
+
+  std::cout << std::endl << " --- ForwardListDemo exit scope --- " << std::endl;
+}
+
+int main(int argc, char const* argv[]) {
+  VectorDemo();
+  DequeDemo();
+  ListDemo();
+  ForwardListDemo();
 }
