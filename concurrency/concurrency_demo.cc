2e60c71b09b8cbc19430beda6763ab9b0f39cda9
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2023-01-17 00:00:15 -0500

    Add ThreadArgsDemo and ThreadCollectionDemo

--- concurrency/concurrency_demo.cc
+++ concurrency/concurrency_demo.cc
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <ranges>
 #include <thread>
+#include <vector>
 
-#include "meta/common/demo_wrapper.h"
-
-auto ThreadDemo = []() {
+void ThreadDemo() {
   std::thread t([]() {
     std::cout << "Hello from thread t: " << std::this_thread::get_id()
               << std::endl;
@@ -11,6 +11,36 @@ auto ThreadDemo = []() {
   t.join();
   std::cout << "Hello from main thread: " << std::this_thread::get_id()
             << std::endl;
+}
+
+auto lambda = [](int x) {
+  std::cout << "thread.get_id(): " << std::this_thread::get_id() << std::endl;
+  std::cout << "Argument x: " << x << std::endl;
 };
 
-int main(int argc, char const* argv[]) { demo::Run("ThreadDemo", ThreadDemo); }
+void ThreadWithArgsDemo() {
+  std::thread t(lambda, 42);
+  t.join();
+  std::cout << "Hello from main thread: " << std::this_thread::get_id()
+            << std::endl;
+}
+
+void ThreadCollectionDemo() {
+  std::vector<std::thread> threads;
+  for (int i = 0; i < 5; i++) threads.emplace_back(lambda, i);
+  for (int i = 0; i < 5; i++) threads[i].join();
+  std::cout << "Hello from main thread: " << std::this_thread::get_id()
+            << std::endl;
+}
+
+// void JThreadDemo() {
+//   std::vector<std::jthread> jthreads;
+//   std::cout << "Hello from main thread: " << std::this_thread::get_id()
+//             << std::endl;
+// }
+
+int main(int argc, char const* argv[]) {
+  ThreadDemo();
+  ThreadWithArgsDemo();
+  ThreadCollectionDemo();
+}
