60442c2d313a84a387f5eb6ff85936c3ad4502ad
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2023-02-10 02:02:04 -0500

    Update print_container meta functions with C++20 concept

--- meta/common/print_containers.h
+++ meta/common/print_containers.h
@@ -2,6 +2,7 @@
 #define META_COMMON_PRINT_CONTAINERS_H_
 
 #include <algorithm>
+#include <concepts>
 #include <deque>
 #include <forward_list>
 #include <iostream>
@@ -22,9 +23,8 @@ namespace meta::common {
 
 // vector, deque, list, forward_list
 // set, multiset, unordered_set, unordered_multiset
-
 template <typename Container>
-std::enable_if_t<
+concept IsSequenceOrAssociativeContainer =
     std::is_same_v<Container, std::vector<typename Container::value_type>> ||
     std::is_same_v<Container, std::deque<typename Container::value_type>> ||
     std::is_same_v<Container, std::list<typename Container::value_type>> ||
@@ -35,8 +35,25 @@ std::enable_if_t<
     std::is_same_v<Container,
                    std::unordered_set<typename Container::value_type>> ||
     std::is_same_v<Container,
-                   std::unordered_multiset<typename Container::value_type>>>
-Print(const std::string_view name, const Container &container) {
+                   std::unordered_multiset<typename Container::value_type>>;
+
+// map, multimap, unordered_map, unordered_multimap
+template <typename Container>
+concept IsUnorderedAssociativeContainer =
+    std::is_same_v<Container, std::map<typename Container::key_type,
+                                       typename Container::mapped_type>> ||
+    std::is_same_v<Container, std::multimap<typename Container::key_type,
+                                            typename Container::mapped_type>> ||
+    std::is_same_v<Container,
+                   std::unordered_map<typename Container::key_type,
+                                      typename Container::mapped_type>> ||
+    std::is_same_v<Container,
+                   std::unordered_multimap<typename Container::key_type,
+                                           typename Container::mapped_type>>;
+
+template <typename Container>
+std::enable_if_t<IsSequenceOrAssociativeContainer<Container>> Print(
+    const std::string_view name, const Container &container) {
   std::cout << name << " { ";
   for (const auto &element : container) {
     if constexpr (std::is_same_v<typename Container::value_type,
@@ -49,21 +66,9 @@ Print(const std::string_view name, const Container &container) {
   std::cout << "}" << std::endl;
 }
 
-// map, multimap, unordered_map, unordered_multimap
-
 template <typename Container>
-std::enable_if_t<
-    std::is_same_v<Container, std::map<typename Container::key_type,
-                                       typename Container::mapped_type>> ||
-    std::is_same_v<Container, std::multimap<typename Container::key_type,
-                                            typename Container::mapped_type>> ||
-    std::is_same_v<Container,
-                   std::unordered_map<typename Container::key_type,
-                                      typename Container::mapped_type>> ||
-    std::is_same_v<Container,
-                   std::unordered_multimap<typename Container::key_type,
-                                           typename Container::mapped_type>>>
-Print(const std::string_view name, const Container &container) {
+std::enable_if_t<IsUnorderedAssociativeContainer<Container>> Print(
+    const std::string_view name, const Container &container) {
   std::cout << name << " {" << std::endl;
   for (const auto &element : container)
     std::cout << "  " << element.first << ": " << element.second << std::endl;
